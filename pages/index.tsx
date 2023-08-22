a11b8b7cde098cb38d9280307976fc10cad8d4c6
StacyFranco <stacyfranco@hotmail.com>
2023-08-22 11:25:45 -0300

    componentes: avatar, botao, uploadeImagem. Variaveis e validadores

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
-
-
+import Botao from '../componentes/botao'
+import Avatar from '../componentes/avatar'
+import {UploadImagem} from '../componentes/uploadImagem'
+import {useRef, useState} from 'react'
 
 
 
 export default function Home() {
+  const [imagem,setImagem] = useState(null);
+  const referenciaInput = useRef(null);
+
   return (
     <>
       <h1>Ola Mundo</h1>
+      <button onClick={() => referenciaInput?.current?.click()}>abrir seletor de arquivos</button>
+
+      <UploadImagem 
+        setImagem={setImagem} 
+        imagemPreview = {imagem?.preview}
+        aoSetarAReferencia={(ref)=> referenciaInput.current=ref} 
+      />
+
+      <Avatar/>
+
+      <Botao texto={'Login'} cor='primaria' manipularClick={() => console.log('botao clicado')} />
     </>
   )
 }
